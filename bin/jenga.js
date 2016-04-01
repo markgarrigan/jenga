31f006fed82489d8814f97599b184f9eafa9de4c
Mark Garrigan <mark@markgarrigan.com>
2016-04-01 07:52:05 -0500

    Add options, help, and more logic

--- bin/jenga.js
+++ bin/jenga.js
@@ -1,22 +1,59 @@
 #! /usr/bin/env node
 require('shelljs/global');
 
-mkdir('-p','dist');
+var argv = require('yargs')
+    .usage('Usage: $0 [options]')
+    .default({ s : 'src', d : 'dist'})
+    .alias('s', 'source')
+    .describe('s', 'Set the source directory')
+    .alias('d', 'dest')
+    .describe('d', 'Set the destination directory')
+    .help('h')
+    .alias('h', 'help')
+    .argv,
+    src = remove_trailing_slashes(argv.s),
+    dest = remove_trailing_slashes(argv.d);
 
-var files = ls('src/*.html');
-var regexp = /<!--include (.*)-->/;
+if (src == '.') {
+  console.log('Jenga can not build html files from the root of your project.');
+  return;
+}
 
-for(var i = 0; i < files.length; i++) {
-  var filename = files[i].split('/');
-  if ( filename[filename.length-1].charAt(0) == '_' ) {
-    continue;
+if (test('-d', src)) {
+  var paths = find(src).filter(function(file) { return file.match(/\.html$/); });
+
+  if (paths.length) {
+    mkdir('-p', dest);
+
+    var regexp = /<!--include (.*)-->/;
+
+    for(var i = 0; i < paths.length; i++) {
+      var path_parts = paths[i].split('/'),
+          filename = path_parts[path_parts.length-1],
+          tempfile = Math.floor(Math.random() * 10000000) + '.tmp';
+      path_parts.shift();
+      path_parts.pop();
+      var path = path_parts.join('/');
+      if ( filename.charAt(0) == '_' ) {
+        continue;
+      }
+      cp(paths[i], tempfile);
+      sed('-i', regexp, function(sedfound) {
+        var match = regexp.exec(sedfound),
+            partial = src + '/' + match[1];
+        if ( !test('-f', partial) ) return sedfound;
+        return cat(partial);
+      }, tempfile);
+      mkdir('-p', dest + '/' + path);
+      mv(tempfile, dest + '/' + path + '/' + filename);
+    }
+  } else {
+    console.log('There are no files in ' + src + ' to build.');
   }
-  cp(files[i], 'tmp.html');
-  sed('-i', regexp, function(something) {
-    var match = regexp.exec(something);
-    var partial = 'src/' + match[1];
-    if ( !test('-f', partial) ) return something;
-    return cat(partial);
-  }, 'tmp.html');
-  mv('tmp.html', 'dist/' + filename[filename.length-1]);
+} else {
+  console.log(src + ' is not a directory.');
+}
+
+function remove_trailing_slashes(string) {
+  return string.replace(/\/+$/, '');
 }
